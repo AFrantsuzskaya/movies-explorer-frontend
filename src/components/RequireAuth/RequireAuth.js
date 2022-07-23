0fff15286acb79e6cfe5fa64d0a08369c0fc9892
Anastasiya Frantsuzskaya <anastasiafrantsuzskaya@yandex.ru>
2022-07-24 02:16:53 +0300

    prep to pull

--- src/components/RequireAuth/RequireAuth.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-import { Navigate, Route } from 'react-router-dom';
-import getLoad_loggedIn from '../App/App';
-export default function RequireAuth({loggedIn, children}) {
-  
-  return loggedIn ? children : <Navigate to="/signin" />
-}
--- src/utils/constants.js
+++
@@ -1,1 +0,0 @@
-export const MOVIES_URL = 'https://api.nomoreparties.co';
